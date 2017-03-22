6c5ae928292be98f2b85921f525498dad8e8f1fe
simonscheider <simonscheider@web.de>
2017-03-22 19:56:01 +0100

    repair network

---
+++ ReparationofFietsersbondNW/repairnetwork.py
@@ -0,0 +1,27 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      simon
+#
+# Created:     22/03/2017
+# Copyright:   (c) simon 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+
+
+def buildNodeList(network):
+
+
+
+
+
+def main():
+    network = "networkProjected.shp"
+    buildNodeList(network)
+
+
+if __name__ == '__main__':
+    main()
